efd28743b670a47303a747e1bb93bd01e636fc86
André 'Dezzy <andrevcv1@gmail.com>
2020-12-23 10:46:29 -0300

    chore: Change puppeteer browser launcher args

--- packages/robot/_shared/src/modules/browser/providers/BrowserProvider/implementations/PuppeteerBrowserProvider.ts
+++ packages/robot/_shared/src/modules/browser/providers/BrowserProvider/implementations/PuppeteerBrowserProvider.ts
@@ -7,20 +7,8 @@ import IBrowserProvider from '../models/IBrowserProvider';
 class PuppeteerBrowserProvider implements IBrowserProvider<Browser> {
   public async launch(options?: LaunchOptions): Promise<Browser> {
     const browser = await puppeteer.launch({
-      product: 'chrome',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
       ignoreHTTPSErrors: true,
-      args: [
-        '--lang=en',
-        '-wait-for-browser',
-        '--ignore-certificate-errors',
-        '--enable-features=NetworkService',
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-web-security',
-        '--disable-features=IsolateOrigins,site-per-process',
-        '--shm-size=3gb',
-      ],
       ...options,
     });
 
